75c5e137ca02ee8c19c9db4b13378e61902e5ef0
Steban Pineda <pinedasteban13@gmail.com>
2026-01-22 22:18:32 -0500

    updated

--- app/Http/Requests/Api/V1/MatriculaUpdateRequest.php
+++ app/Http/Requests/Api/V1/MatriculaUpdateRequest.php
@@ -30,7 +30,7 @@ class MatriculaUpdateRequest extends FormRequest
             'numero_documento' => ['sometimes', 'string', 'max:255', Rule::unique('matriculas', 'numero_documento')->ignore($cod, 'cod_alumno')],
             'lugar_expedicion_documento' => ['nullable', 'string', 'max:255'],
             'tipo_documento' => ['sometimes', 'in:CC,TI,PPT'],
-            'fecha_nacimiento' => ['sometimes', 'date'],
+            'fecha_nacimiento' => ['sometimes', 'nullable', 'date'],
             'direccion_barrio' => ['nullable', 'string', 'max:255'],
             'ciudad_residencia' => ['nullable', 'string', 'max:255'],
             'departamento' => ['nullable', 'string', 'max:255'],
